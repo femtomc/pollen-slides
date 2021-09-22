6cbb47b10752e36796e8cbefcdd628b9ac685e5b
femtomc <mccoybecker@gmail.com>
2021-09-22 14:46:11 -0400

    Rebuilding example - Wed Sep 22 02:46:11 PM EDT 2021

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 cd pollen
 raco pollen reset
 rm css/*.css
+rm *.html
 rm -rf images
 rm -rf latex
 raco pollen render -r .
 raco pollen publish . $(pwd)/../example
 raco pollen reset
+rm css/*.css
+rm *.html
+rm -rf images
+rm -rf latex
 cd ..
 msg="Rebuilding example - $(date)"
 if [ -n "$*" ]; then
