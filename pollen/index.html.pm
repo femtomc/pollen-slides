0eefb9311b978f44c60d0d84700af0dca36f5b5a
femtomc <mccoybecker@gmail.com>
2021-09-29 22:31:29 -0400

    Rebuilding example - Wed Sep 29 10:31:29 PM EDT 2021

--- pollen/index.html.pm
+++ pollen/index.html.pm
@@ -16,6 +16,42 @@ class: center, middle
 ◊; --- is a separator which indicates a new slide should begin.
 ---
 
+## Motivation
+
+**Why do we care about verification of probabilistic programs (let alone higher-order ones) at all?**
+
+______
+
+* Modeling and inference _is hard_ - why make it harder by allowing unsound semantics when we can afford to check?
+
+◊img[#:style "display:block;margin-left:auto;margin-right:auto;max-width:450px;" #:src "assets/img/ttmotivation.png"]
+
+---
+
+* Think: tensor shape checkers for neural network specification languages (think: PyTorch, TensorFlow, Dex, etc) - what is the equivalent for probabilistic programming?
+
+.cols[
+.fifty[
+◊img[#:style "display:block;margin-left:auto;margin-right:auto;max-width:300px;" #:src "assets/img/autostat.png"]
+]
+.fifty[
+1. ***How can we be sure the automatic statistician is correct?***
+
+2. ***What if it uses provably invalid inference algorithms?***
+
+3. ***Would you trust an AI system without provably correct inference?***
+]
+]
+
+______
+
+* How can you be sure a static tool works for all programs without a proof?
+    * Map your language to a denotational space (a mathematical space) - map transformations described by your language to reasoning principles in the denotational space.
+
+** An (unfortunate) theme: difficult-to-describe features (automatic differentiation, probabilistic programming) map to somewhat complex categories (differentiable manifolds, quasi-Borel spaces).**
+
+---
+
 ◊; This is a small Remark.js feature -- you can use . syntax to specify blocks.
 ◊; So below is a convenient hack to put in columns.
 .cols[
@@ -34,7 +70,7 @@ ______
 
 5. Why is this useful?
 
-◊${\rightarrow} The goal is not to prove everything rigorously - but to support your own understanding of the story (and encourage your own exploration).
+* The goal is not to prove everything rigorously - but to support your own understanding of the story (and encourage your own exploration).
 
 ______
 
@@ -81,7 +117,7 @@ To formalize this process using mathematics, we must turn to measure theory - be
 
 ## Probabilistic programming
 
-◊${\rightarrow} Understanding computable representions of operations on measures.
+* Understanding computable representions of operations on measures.
 
 ______
 
@@ -116,6 +152,8 @@ end
 ______
 
 A _generative function_ is a type of computational object which supports a well-defined interface.
+
+Roughly, how do I sample, evaluate density at a point, get gradients with respect to parameters, and gradients with respect to density.
 
 .cols[
 .fifty[
@@ -155,7 +193,16 @@ What does "higher-order" mean in the context of programs which denote measures a
 ]
 ]
 
-______
+.cols[
+.fifty[
+◊img[#:style "display:block;margin-left:auto;margin-right:auto;max-width:300px;" #:src "assets/img/anglican_linreg.png"]
+]
+.fifty[
+<br>
+
+`f` denotes a measure over functions ... _what does that even mean?_
+]
+]
 
 ---
 
@@ -187,7 +234,7 @@ ______
 ]
 ]
 
-Informal: a _category_ is a collection of objects with arrows between them. The arrows satisfy nice properties:
+Informal: a _category_ is a collection of objects with arrows (called _morphisms_) between them. The arrows satisfy nice properties:
 
 1. If an arrow goes into another arrow, the composite arrow is in the category.
 2. Arrows are fully associative.
@@ -201,9 +248,41 @@ Such a pattern is typically called a _universal construction_.
 
 ---
 
+Let's look at an example of a _universal construction_.
+
+______
+
+.cols[
+.fifty[
+### Initial object
+
+An object `I` in a category `C` so that, for every object `X` in `C`, there is exactly one morphism `I -> X`.
+]
+
+.fifty[
+### Terminal object
+
+An object `I` in a category `C` so that, for every object `X` in `C`, there is exactly one morphism `X -> I`.
+]
+]
+
+______
+
+Let's consider the category of sets `Set`:
+
+1. Terminal objects are any singleton set `Unit` with lone element `T`.
+    * For every set `S`, there is exactly one morphism (in `Set`, one function) which maps a set to `Unit`.
+    * Unique (up to isomorphism) -- because any singleton is isomorphic to any other.
+
+2. The initial object is the empty set `{}`.
+    * Why? Think about functions (again, because we are in `Set`!) as choosing subsets of Cartesian product of two sets.
+    * The empty set is the only initial object in `Set`.
+
+---
+
 Generally, computer scientists concern themselves with _Cartesian closed categories_.
 
-We typically like to think that our programs denote objects + transformations in some mathematical space. This gets tricky when the transformations themselves can be objects.
+We typically like to think that our programs denote objects + transformations in some mathematical space. One might ask: how do we model function application?
 
 ______
 
@@ -225,11 +304,11 @@ The technical definition is:
 
 ______
 
-If a category has this property, it can be used to model (denotationally) higher-order semantics of programming languages.
+If a category has this property, it can be used to model (denotationally) function application in programming languages.
 
 In a moment, we'll understand this intuitively.
 
-◊${\rightarrow} Further: [(John Baez) CCCs and the ◊${\lambda}-calculus](https://golem.ph.utexas.edu/category/2006/08/cartesian_closed_categories_an_1.html)
+* Further: [(John Baez) CCCs and the ◊${\lambda}-calculus](https://golem.ph.utexas.edu/category/2006/08/cartesian_closed_categories_an_1.html)
 
 ---
 
@@ -241,7 +320,7 @@ ______
 ◊; The dot command allows usage of the dot graph language inline.
 ◊dot{
 ep[label="Category theory"];
-en[label="Lambda calculi"]
+en[label="STLC"]
 ep->en;
 en->ep;
 }
@@ -279,11 +358,11 @@ c -> b [label="q"];
 ◊dot{
 rankdir=LR;
 label="Coproduct";
-a -> c;
-b -> c;
-c -> cp;
-a -> cp;
-b -> cp;
+a -> c[label="p"];
+b -> c[label="q"];
+c -> cp[label="m"];
+a -> cp[label="p'"];
+b -> cp[label="q'"];
 }
 ]
 ]
@@ -302,6 +381,7 @@ A -> B;
 }
 ]
 .fifty[
+#### Morphisms
 In category theory, a mapping between two objects is called a _morphism_.
 ]
 ]
@@ -349,7 +429,7 @@ ______
 
 Remember: the way to think about this sort of construction is as a pattern which may or may not "match" to the category in question.
 
-◊${\rightarrow} **It's not important to fully understand this now.**
+* **It's not important to fully understand this now.**
 
 More important: understanding that there's a way to identify categories with this object, and, even more important, a way to identify when a category contains this object as a base object in the category.
 
@@ -451,6 +531,23 @@ A quasi-Borel space ◊${X} consists of an underlying set ◊${X} and a set of f
     }
 }
 
+---
+
+The key to understanding quasi-Borel spaces is to understand the role that ◊${\sigma}-algebras play in measure theory.
+
+______
+
+1. When you select a ◊${\sigma}-algebra - it is equivalent to selecting "predicates" which you can ask the probability of. If you try to ask a question "what is the probability of this event occurring" and it is not in the selected ◊${\sigma}-algebra - the predicate cannot be answered (the set is not measurable).
+
+2. How do you assign a ◊${\sigma}-algebra to the set denoted by function type `X -> Y`? There is no ◊${\sigma}-algebra on `X -> Y` which makes function application measurable (where `apply :: (X, X -> Y) -> Y`).
+
+______
+
+To avoid this problem, the quasi-Borel proposal is: 
+
+1. Let's avoid talking about ◊${\sigma}-algebras on our space `X`, and instead talk about "admissible random elements" from a well-behaved measure space ◊${\mathbb{R}} to our space. 
+
+2. These functions are now like the predicates above -- a deterministic function on `X` is measurable _iff_ composing it with an admissible random element yields another admissible random element.
 
 ---
 
@@ -463,6 +560,8 @@ ______
 From this contribution statement, we should essentially be expecting two things:
 
 1. A simply typed lambda calculus (STLC) whose denotational semantics are given by the category of quasi-Borel spaces `QBS`. This will be the base language.
+    * `QBS` supports product objects, function objects, and is Cartesian closed - so it is "`Set`-like" and supports denotational reasoning about higher-order functions!
+    * There's also a functor from the category `Meas` to `QBS`. See [A Convenient Category for Higher-Order Probability Theory](https://arxiv.org/pdf/1701.02547.pdf) (1.A)
 
 2. A logic which quantizes over expressions in the STLC. This will be the system which reasons about programs in the base language.
 
@@ -484,7 +583,7 @@ data Expr =   -- Variables, builtins, and application.
                 x | c | f | Expr Expr
 
               | <Expr, Expr> -- A product constructor.
-              | Project(i, Expr) -- Record type field projection.
+              | Project(i, Expr) -- Product destructor.
               
               -- Pattern matching.
               | case Expr with [match(i, x_i) => Expr] over i
@@ -503,12 +602,14 @@ data Expr =   -- Variables, builtins, and application.
 
 ---
 
-The language `HPPROG` is a typed lambda calculus with products, pattern matching, recursive function definitions, and monadic binding.
+The language `HPPROG` is a typed lambda calculus with products, pattern matching, recursive function definitions, and monadic lifting/binding as well as conditioning.
 
 1. `Product -> <Expr ,Expr>`
 2. `Pattern matching -> case Expr with [match(i, x_i) => Expr] over i`
 3. `Recursive function definitions -> letrec f x = Expr`
+3. `Monadic lift -> return Expr` with monadic type `M[T]`.
 4. `Monadic bind -> bind Expr Expr` with monadic type `M[T]`
+5. `Query -> query Expr Expr` with resultant monadic type `M[T]`.
 
 ______
 
@@ -523,8 +624,6 @@ B -> C [constraint="false", label="(>>=)"];
 }
 ]
 .sixty[
-A <code>Monad</code> is a map from category to category, constrained to satisfy a certain set of laws.
-
 The monadic bind operation chains together computations in the monadic context.
 
 ```haskell
@@ -536,8 +635,7 @@ The monadic bind operation chains together computations in the monadic context.
 
 ______
 
-* Handling probability with a monadic type `M[T]` is relatively standard, see: [monads of probability, measures, and valuations](https://ncatlab.org/nlab/show/monads+of+probability%2C+measures%2C+and+valuations) and [the Giry monad](https://ncatlab.org/nlab/show/Giry+monad).
-* The QBS paper develops generalizations of the Giry monad to represent the monadic `X -> QBS(X)`.
+* Handling measures with a monadic type `M[T]` is relatively standard, see: [monads of probability, measures, and valuations](https://ncatlab.org/nlab/show/monads+of+probability%2C+measures%2C+and+valuations) and [the Giry monad](https://ncatlab.org/nlab/show/Giry+monad).
 
 ---
 
@@ -577,14 +675,14 @@ E.g. in the `Maybe` monad, the process is: map a computation over type `T` to a 
 
 ```haskell
 -- A.k.a. terms.
-data ExtE = Expr 
-          | E(x, ExtE, Expr(x)) 
-          | scale(ExtE, ExtE)
-          | normalize(ExtE)
+data EnrE = Expr 
+          | E(x, EnrE, Expr(x)) 
+          | scale(EnrE, EnrE)
+          | normalize(EnrE)
 
 -- A.k.a formula.
-data LogF = (ExtE = ExtE) 
-          | (ExtE < ExtE)
+data LogF = (EnrE = EnrE) 
+          | (EnrE < EnrE)
           | Top
           | Btm
           | LogF & LogF
@@ -596,19 +694,108 @@ data LogF = (ExtE = ExtE)
 
 ---
 
-## Gaussian mean learning
+Let's quickly look at the extended typing rules for enriched expressions.
+
+.cols[
+.fifty[
+◊img[#:style "display:block;margin-left:auto;margin-right:auto;max-width:300px;" #:src "assets/img/typing_expect.png"]
+<br>
+
+◊img[#:style "display:block;margin-left:auto;margin-right:auto;max-width:300px;" #:src "assets/img/typing_scale.png"]
+<br>
+
+◊img[#:style "display:block;margin-left:auto;margin-right:auto;max-width:300px;" #:src "assets/img/typing_normalize.png"]
+]
+
+.fifty[
+1. Integrating an always positive measurable function with a measure always yields a positive ◊${\mathbb{R}} number.
+2. Scaling a measure by an always positive measurable function yields a measure.
+3. Normalizing a measure converts it into a probability measure (which is still a measure).
+]]
 
 ______
 
-Let's now examine one of the paper examples and try and put the pieces together.
+I'm using "measure" above loosely - the denotation of `M[T]` means that this is a quasi-Borel measure: an equivalence class of admissible random elements.
 
-______
-
-```haskell
-GaussLearn = 
-    \ p . letrec f(L) = case L of
-            [] -> p,
-            y :: rest -> query f(ls) => GPDF(y, sigma^2)
-```
+This fact is irrelevant to the typing - because these theorems also hold in QBS.
 
 ---
+
+We can also quickly look at the selection of proof rules:
+
+______
+
+◊img[#:style "display:block;margin-left:auto;margin-right:auto;max-width:700px;" #:src "assets/img/pl_proof_rules.png"]
+
+______
+
+(From top-left, clockwise)
+
+1. (?) This honestly confused me. I think this is showing that two terms are equivalent after reduction - we can conclude they are equivalent and add that to the precondition context
+
+2. Proving that a term `t = u` implies that we can swap it in substitution for a formula ◊${\phi}.
+
+3. If we have ◊${\psi => \phi} in the precondition, and we also have ◊${\psi}, we can conclude that we have ◊${\phi}. This is using an implication.
+
+4. This rule allows us to add an implication.
+
+5. This rule is a rule about axioms.
+
+---
+
+There's a large section where the authors essentially re-state many theorems of classical probability using the constructs of `PL`:
+
+◊img[#:style "display:block;margin-left:auto;margin-right:auto;max-width:450px;" #:src "assets/img/prob_axioms.png"]
+
+---
+
+.cols[
+.fifty[
+#### Unary
+]
+.fifty[
+#### Relational
+]
+]
+
+______
+
+One really nice thing: the pure/probabilistic constructions separate over the monadic constructs!
+
+.cols[
+.fifty[
+◊img[#:style "display:block;margin-left:auto;margin-right:auto;max-width:400px;" #:src "assets/img/upl_rules.png"]
+
+At a high level, the unary logic `UPL` allows singular judgements about a well-typed expression `e : T`. The relational logic deals with judgements on pairs of well-typed `HPProg` expressions.
+]
+
+.fifty[
+◊img[#:style "display:block;margin-left:auto;margin-right:auto;max-width:400px;" #:src "assets/img/rpl_rules.png"]
+]
+]
+
+---
+
+#### Strengths
+
+______
+
+1. A nice example of a logic which allows reasoning over a language with higher-order functions and probabilistic conditioning (on real and discrete) random variables!
+
+2. Logic rules nicely separate across monad.
+
+3. Logic subsystems are modular. The UPL is designed to handle judgements of a certain type, similarly for the RPL.
+
+4. Importantly -- it's a step in the right direction. More people should really be thinking about this in probabilistic programming.
+
+---
+
+#### Weaknesses
+
+______
+
+1. Base language is clunky -- you always have to setup a product variable and query via projections on that product. Hopefully this could be sugared up in some way. In short, it's not designed for writing complex, modular models.
+2. I was unimpressed by the examples (with the exception of Lipschitz GVI verification), for a variety of reasons.
+    * Slicing example is absolutely useless in practice. Unless the proof can be automated across different structured programs, it seems like an exercise in futility.
+    * Gaussian mean learning is hardly a useful example to test a framework like this on. E.g. they coerce the program into higher-order form ... but it could easily be expressed without higher-order functions.
+    * I actually did like the self-normalizing importance sampling example. I think it illustrated a principle which I would have liked to see more: relationaly judgements which are parametric over one of the expressions -- here, the result is a bound which depends on difference between empirical (e.g. sampled mean) and target (see [Chatterjee + Diaconis](https://projecteuclid.org/journals/annals-of-applied-probability/volume-28/issue-2/The-sample-size-required-in-importance-sampling/10.1214/17-AAP1326.full)).
